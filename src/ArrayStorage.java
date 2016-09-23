68e00b81bd8b5f5d940ffb15f17c2f4eb260626b
Sveta <geksogen4ik@gmail.com>
2016-09-23 22:09:12 +0300

    HW3

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,67 +2,102 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    int size = 0;
-    int storageMaxLenth = 10000;
-    Resume[] storage = new Resume[storageMaxLenth];
+    private int size = 0;
+    private int storageMaxLength = 10000;
 
-    void clear() {
+    private Resume[] storage = new Resume[storageMaxLength];
+
+    public void clear() {
         for (int i = 0; i < size; i++) {
-                storage[i] = null;
-            }
-            size = 0;
+            storage[i] = null;
+        }
+        size = 0;
     }
 
-    void save(Resume r) {
-         if (size == storageMaxLenth) {
-            System.out.println("Хранилище  переполнено!");
+    private int getIndex(String uuid) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if ((storage[i].getUuid().equals(uuid))) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("ERROR: The same " + r + "resume already exists");
             return;
         }
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(r.getUuid())) {
-                System.out.println("Такое " + r + "резюме уже существует");
+
+        if (size == storageMaxLength) {
+            System.out.println("ERROR: Storage is full");
+            return;
+        }
+        {
+
+            storage[size] = r;
+            size++;
+        }
+    }
+
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("ERROR: This " + uuid + " don't exist, if you want to create, click  - save");
+            return null;}
+
+        {
+            return storage[index];
+        }
+
+    }
+
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("ERROR: This " + uuid + " don't exist, if you want to create, click  - save");
+            {
+                storage[index] = storage[size];
+                size--;
+                System.out.println("Resume delete" + uuid);
                 return;
             }
-        }
-        storage[size] = r;
-        size++;
-    }
 
-    Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-                    if (storage[i].getUuid().equals(uuid))
-                    {
-                        return storage[i];
-                    }
-                }
-              System.out.println("Такого " + uuid + " резюме    не существует, если хотите создать нажмите - save");
-        return null;
-    }
-
-    void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[size];
-                storage[size] = null;
-                size--;
-                System.out.println("Резюме удалено " + uuid);
-            } else System.out.println("Такого " + uuid + " резюме не существует, если хотите создать нажмите - save!!!");
-           
         }
     }
+
+
+
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("ERROR: This " + r + " don't exist, if you want to create, click  - save");
+        }
+        storage[index] = r;
+    }
+
+
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-          Resume[] resumes = new Resume[size];
-            for (int i = 0; i < size; i++) {
-                resumes[i] = storage[i];
-            }
-            return resume;
+    public Resume[] getAll() {
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
-    int size() {
+    public int size() {
+
 
         return size;
     }
