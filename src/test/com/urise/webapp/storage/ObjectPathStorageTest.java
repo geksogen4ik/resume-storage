775cb47db69b08a3d76b1aa9af924e34138ff099
Sveta <geksogen4ik@gmail.com>
2016-11-03 21:25:33 +0200

    HW10

--- src/test/com/urise/webapp/storage/ObjectPathStorageTest.java
+++ src/test/com/urise/webapp/storage/ObjectPathStorageTest.java
@@ -12,6 +12,7 @@ import com.urise.webapp.storage.serializer.ObjectStreamSerializer;
  */
 public class ObjectPathStorageTest extends AbstractStorageTest {
     public ObjectPathStorageTest(){
+
         super(new PathStorage(STORAGE_DIR, new ObjectStreamSerializer()));
     }
 }
